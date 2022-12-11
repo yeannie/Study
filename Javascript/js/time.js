55af4c8a6f5db300f2ce65d823aa00fed22a7d2f
yeannie <96753951+yeannie@users.noreply.github.com>
2022-12-11 17:56:58 +0900

    Delete Javascript directory

--- Javascript/js/time.js
+++
@@ -1,7 +0,0 @@
-var head = document.querySelector("#head");
-head.onclick = function(){
-    head.style.color = "red";
-}
-var now = new Date();
-var disp = now.toLocaleTimeString();
-document.write("현재시각"+disp);
